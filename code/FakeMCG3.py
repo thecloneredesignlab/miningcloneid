dd24119969dc490bcefabb467a9df129e157cc8a
NatalieMeacham <118575832+NatalieMeacham@users.noreply.github.com>
2025-11-06 14:35:30 -0500

    Update FakeMCG3.py
    start making figures

--- code/FakeMCG3.py
+++ code/FakeMCG3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import random
 from PujanEarlyVersionModel import ploidy_forcast
 from time import time
@@ -28,8 +29,6 @@ def simulate_next_state(ploidy_status, drug):
     y= T_mat_ode.T #do stuff on tmatode
     new_status = {states[k]: float(y[d_switch][k]) for k in range(len(states))}
     return new_status  # negative burden = reward
-
-
 def select_best_child(node, c):
     best_score, best_child = -float('inf'), None
     for drug, child in node.children.items():
@@ -59,8 +58,6 @@ def rollout(node, rollout_depth):
         ploidy = simulate_next_state(ploidy, drug)
     final_burden = sum(ploidy.values())
     return -final_burden
-
-
 def backpropagate(node, reward):
     while node is not None:
         node.N += 1
@@ -82,13 +79,18 @@ total_cycles = 11
 min_size = 1e8
 max_size = 2e10
 depth = 30
-num_rollouts = 100000
+num_rollouts = 10000 #should be 100000
 decay_factor = 0.5  # After a few cycles, old stats are negligible
 c=1.4  # Exploration parameter; higher values encourage exploration of less-visited nodes
 
-ploidy_status = {2: 0.7*1e9, 4: 0.3*1e9}
-root = Node(ploidy_status, 0)
+#number of cycles counter
+cycle_counter=0
 
+ploidy_status = {2: 0.7*1e9, 4: 0.3*1e9}
+
+best_drug_list = []
+
+root = Node(ploidy_status, 0)
 for decision in range(total_cycles):
     for _ in range(num_rollouts):  # number of MCTS simulations per decision
         node = root
@@ -109,6 +111,9 @@ for decision in range(total_cycles):
     # Pick best drug for this decision
     best_drug = max(root.children.items(), key=lambda kv: kv[1].W / (kv[1].N + 1e-6))[0]
     print(f"Cycle {decision + 1}: best drug is {best_drug}")
+    best_drug_list.append(best_drug)
+
+    cycle_counter=cycle_counter + 1
 
     # Move environment forward
     ploidy_status = simulate_next_state(ploidy_status, best_drug)
@@ -122,7 +127,41 @@ for decision in range(total_cycles):
     elif sum(ploidy_status.values()) > max_size:
         print("Tumor burden exceeded 2000 mm3, stopping treatment.")
         break
-
 print(f"Final tumor burden: {sum(ploidy_status.values())}")
 end = time()
 print(f"Total time taken: {end - start:.2f} seconds")
+print(cycle_counter)
+print(best_drug_list)
+print(ploidy_status)
+
+# print(best_drug_list[3])
+
+plt.figure(figsize=(8, 6)) # Create a single figure
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Multiple Lines on One Plot")
+
+for b in range(cycle_counter):
+    #simulate fp for given ploidy and drug
+    drug=best_drug_list[b]
+    ploidies, t_ode,T_mat_ode, _, _ = ploidy_forcast(ploidy_status, drug, d_switch)
+    # ploidy_status=ploidies
+    total_vol = np.squeeze(np.sum(T_mat_ode,axis=0))
+    tvec=t_ode+ b*d_switch
+    plt.yscale('log')
+    plt.plot(tvec, total_vol, label=f"Line {b+1}")
+    # a=T_mat_ode[0,-1]/sum(T_mat_ode[:,-1])
+    # c=T_mat_ode[1,-1]/sum(T_mat_ode[:,-1])
+    ploidy_status={ploidies[0]:T_mat_ode[0,-1], ploidies[1] :T_mat_ode[1,-1]}
+
+plt.show()
+
+print(ploidies)
+print(T_mat_ode)
+# print(a)
+# print(c)
+print(total_vol)
+print(t_ode)
+print(tvec)
+print(len(tvec))
+print(len(total_vol))
